d05f8aa5d7dd71dec9315d8fe96adf836e5f8e7c
cabraljv <joaovictorcabral.dev@gmail.com>
2020-06-07 23:39:09 -0300

    start map in the user position

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,6 +1,8 @@
-import React, {useRef, useEffect} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 import {StatusBar} from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
+
 import {
   Container,
   OpenDrawerButton,
@@ -18,7 +20,6 @@ import {
   CloseModalButton,
 } from './styles';
 import {Modalize} from 'react-native-modalize';
-import {Text, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {MAPBOX_KEY} from 'react-native-dotenv';
 interface Props {
@@ -28,13 +29,21 @@ interface Props {
 MapboxGL.setAccessToken(MAPBOX_KEY);
 MapboxGL.setConnected(true);
 const Dashboard: React.FC<Props> = ({navigation}) => {
+  const [coords, setCoords] = useState([0, 0]);
   const modalizeRef = useRef<Modalize>(null);
   useEffect(() => {
     MapboxGL.setTelemetryEnabled(false);
+    Geolocation.getCurrentPosition(
+      (position) => {
+        setCoords([position.coords.longitude, position.coords.latitude]);
+      },
+      (error) => console.log(error),
+      {timeout: 5000}
+    );
   }, []);
-  const onOpen = () => {
+  /* const onOpen = () => {
     modalizeRef.current?.open();
-  };
+  };*/
   return (
     <Container>
       <StatusBar backgroundColor="transparent" translucent />
@@ -47,8 +56,10 @@ const Dashboard: React.FC<Props> = ({navigation}) => {
         logoEnabled={false}
         styleURL="mapbox://styles/cabraljv/ckan4arq92git1inyy1ezq4jb"
         attributionEnabled={false}
-        compassViewPosition={3}
-      />
+        compassViewPosition={3}>
+        <MapboxGL.Camera centerCoordinate={coords} zoomLevel={14} />
+        <MapboxGL.UserLocation />
+      </MapboxGL.MapView>
       <Modalize
         ref={modalizeRef}
         scrollViewProps={{showsVerticalScrollIndicator: false}}
