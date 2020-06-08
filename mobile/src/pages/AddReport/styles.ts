c54b2d19fb26d4a3ac2691547693ee425be55029
cabraljv <joaovictorcabral.dev@gmail.com>
2020-06-08 01:23:56 -0300

    add picker to report screen

---
+++ mobile/src/pages/AddReport/styles.ts
@@ -0,0 +1,66 @@
+import styled from 'styled-components/native';
+import {Dimensions} from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+export interface Props {
+  isImage?: boolean;
+}
+export const Container = styled.ScrollView`
+  flex: 1;
+  background: #fff6f6;
+`;
+export const Header = styled.View`
+  flex-direction: row;
+  margin: 0 30px;
+  margin-top: 10px;
+  margin-bottom: 20px;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const Title = styled.Text`
+  font-family: 'NotoSans-Bold';
+  font-size: 26px;
+  width: 50%;
+  color: #ff5f5f;
+`;
+export const HeaderImage = styled.Image``;
+export const BackButton = styled.TouchableOpacity`
+  align-self: flex-start;
+  margin: 5px 30px;
+  margin-top: 40px;
+`;
+export const ItemField = styled.View`
+  margin: 0 ${(props: Props) => (props.isImage ? 'auto' : '30px')};
+  margin-bottom: 30px;
+`;
+export const Label = styled.Text`
+  font-family: 'NotoSans-Bold';
+  font-size: 12px;
+  color: #ff5f5f;
+`;
+export const ImageField = styled.TouchableOpacity`
+  width: ${0.7 * windowWidth}px;
+  height: ${0.7 * windowWidth}px;
+  border: 3px solid #ff5f5f;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+`;
+export const AddImageIcon = styled.Image``;
+export const ImagePreviewItem = styled.Image`
+  width: ${0.7 * windowWidth}px;
+  height: ${0.7 * windowWidth}px;
+`;
+export const DescriptionInput = styled.TextInput`
+  width: 100%;
+  height: 170px;
+  background: #fff;
+  elevation: 5;
+  padding: 10px;
+  border-radius: 10px;
+`;
+export const ReportPickerContainer = styled.View`
+  border: 2px solid #ff5f5f;
+  border-radius: 8px;
+  padding: 2px;
+`;
